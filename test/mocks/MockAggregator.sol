8c0092dd428d22f2bfa6d2e5a547b36b158e9ca2
Pavel Naydanov <pavel.naydanov@metalamp.io>
2026-06-04 17:05:28 +0700

    test: realize MockAggregator

---
+++ test/mocks/MockAggregator.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {AggregatorV3Interface} from "src/interfaces/AggregatorV3Interface.sol";
+
+/// @dev Minimal Chainlink AggregatorV3 mock for tests. Allows setting price and updatedAt
+contract MockAggregator is AggregatorV3Interface {
+    uint8 private immutable _DECIMALS;
+
+    uint80 private _roundId;
+    int256 private _answer;
+    uint256 private _startedAt;
+    uint256 private _updatedAt;
+    uint80 private _answeredInRound;
+
+    constructor(uint8 initialDecimals, int256 initialAnswer) {
+        _DECIMALS = initialDecimals;
+        _roundId = 1;
+        _answer = initialAnswer;
+        _startedAt = block.timestamp;
+        _updatedAt = block.timestamp;
+        _answeredInRound = 1;
+    }
+
+    /// @dev Bumps round, sets answer, updatedAt = now
+    function setAnswer(int256 answer) external {
+        _roundId += 1;
+        _answer = answer;
+        _startedAt = block.timestamp;
+        _updatedAt = block.timestamp;
+        _answeredInRound = _roundId;
+    }
+
+    /// @dev Overrides updatedAt only
+    function setUpdatedAt(uint256 updatedAt) external {
+        _updatedAt = updatedAt;
+    }
+
+    /// @dev Full control: bumps round, sets both answer and updatedAt
+    function setAnswerAt(int256 answer, uint256 updatedAt) external {
+        _roundId += 1;
+        _answer = answer;
+        _startedAt = updatedAt;
+        _updatedAt = updatedAt;
+        _answeredInRound = _roundId;
+    }
+
+    function decimals() external view returns (uint8) {
+        return _DECIMALS;
+    }
+
+    function description() external pure returns (string memory) {
+        return "MockAggregator";
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80)
+        external
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        return (_roundId, _answer, _startedAt, _updatedAt, _answeredInRound);
+    }
+
+    function latestRoundData()
+        external
+        view
+        returns (uint80, int256, uint256, uint256, uint80)
+    {
+        return (_roundId, _answer, _startedAt, _updatedAt, _answeredInRound);
+    }
+}
--- test/utils/BaseTest.sol
+++ test/utils/BaseTest.sol
@@ -4,28 +4,29 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {FullMath} from "@uniswap/v4-core/src/libraries/FullMath.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {CurrencyLibrary, Currency} from "@uniswap/v4-core/src/types/Currency.sol";
-import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
-import {Constants} from "@uniswap/v4-core/test/utils/Constants.sol";
 
-import {Index, AssetConfig, Asset} from "src/Index.sol";
+import {Index, AssetConfig} from "src/Index.sol";
 import {IndexFactory} from "src/IndexFactory.sol";
 import {DriftwoodHook} from "src/DriftwoodHook.sol";
 
 import {MockERC20} from "test/mocks/MockERC20.sol";
+import {MockAggregator} from "test/mocks/MockAggregator.sol";
+
 import {UniswapDeployers} from "./UniswapDeployers.sol";
 
 abstract contract BaseTest is Test, UniswapDeployers {
-    address constant public USDT_DATA_FEED = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
-    address constant public ETH_DATA_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
-
     using SafeERC20 for IERC20;
     using CurrencyLibrary for Currency;
-    using PoolIdLibrary for PoolKey;
+
+    address public usdtFeed;
+    address public ethFeed;
 
     address usdt;
     address weth;
@@ -34,11 +35,9 @@ abstract contract BaseTest is Test, UniswapDeployers {
         _deployUniswapArtifactsAndLabel();
         createdIndex = _deployIndex(defaultAdmin);
 
-        Currency currency0;
-        Currency currency1;
-        if (usdt > weth) {
-            (currency0, currency1) = (Currency.wrap(address(weth)), Currency.wrap(address(usdt)));
-        }
+        (Currency currency0, Currency currency1) = usdt < weth
+            ? (Currency.wrap(usdt), Currency.wrap(weth))
+            : (Currency.wrap(weth), Currency.wrap(usdt));
 
         // Deploy the hook to an address with the correct flags
         address flags = address(
@@ -57,8 +56,11 @@ abstract contract BaseTest is Test, UniswapDeployers {
             tickSpacing: 60,
             hooks: IHooks(createdHook)
         });
-        PoolId poolId = poolKey.toId();
-        poolManager.initialize(poolKey, Constants.SQRT_PRICE_1_1);
+
+        uint160 sqrtPriceX96 = usdt > weth
+            ? _encodeSqrtPriceX96(3000e6, 1e18)  // currency0=WETH, currency1=USDT
+            : _encodeSqrtPriceX96(1e18, 3000e6); // currency0=USDT, currency1=WETH
+        poolManager.initialize(poolKey, sqrtPriceX96);
 
         bytes32 hookRole = createdIndex.HOOK_ROLE();
 
@@ -82,6 +84,7 @@ abstract contract BaseTest is Test, UniswapDeployers {
 
     function _deployIndex(address defaultAdmin) internal returns (Index createdIndex) {
         _deployTokens();
+        _deployFeeds();
 
         AssetConfig[] memory assets = _getAssets();
 
@@ -105,24 +108,45 @@ abstract contract BaseTest is Test, UniswapDeployers {
         tokens = new address[](2);
         tokens[0] = usdt;
         tokens[1] = weth;
+
+        vm.label(usdt, "USDT");
+        vm.label(weth, "WETH");
+    }
+
+    function _deployFeeds() internal {
+        usdtFeed = address(new MockAggregator(8, 1e8)); // $1.00 | feed decimals = 8
+        ethFeed  = address(new MockAggregator(8, 3000e8)); // $3000.00 | feed decimals = 8
     }
 
     function _getAssets() internal view returns (AssetConfig[] memory assets) {
+        require(usdt != address(0) && weth != address(0), "Tokens should be deployed");
+        require(usdtFeed != address(0) && ethFeed != address(0), "Data feeds should be deployed");
+
         assets = new AssetConfig[](2);
         assets[0] = AssetConfig({
             token: usdt,
-            dataFeed: USDT_DATA_FEED,
-            amount: 1_000_0000e6
+            dataFeed: usdtFeed,
+            amount: 3_000_000_000e6,
+            targetWeightBps: 5_000, // 50%
+            toleranceBps: 500, // 5%
+            maxPriceStaleness: 86400
         });
 
         assets[1] = AssetConfig({
             token: weth,
-            dataFeed: ETH_DATA_FEED,
-            amount: 1_000_0000e18
+            dataFeed: ethFeed,
+            amount: 1_000_000e18,
+            targetWeightBps: 5_000, // 50%
+            toleranceBps: 500, // 5%
+            maxPriceStaleness: 3600
         });
     }
 
     function _etch(address target, bytes memory bytecode) internal override {
         vm.etch(target, bytecode);
+    }
+
+    function _encodeSqrtPriceX96(uint256 amount1, uint256 amount0) internal pure returns (uint160) {
+        return uint160(Math.sqrt(FullMath.mulDiv(amount1, 1 << 192, amount0)));
     }
 }
